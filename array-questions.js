8e8c96c17e2f0ef6f3192accd19f7127c1b131b9
Ryan Pedersen <ryan.pedersen@outlook.com>
2020-11-12 10:28:09 -0800

    find sum question

--- array-questions.js
+++ array-questions.js
@@ -1,27 +1,24 @@
 // given input as an array of integers, find the missing integers in the array
 // example: 1,2,100 has missing integers 3 through 99.
 function findMissingIntegers(input) {
-
-    // add logic to find missing integers in input
-
 }
 
 // given input as an array of integers, find the duplicate integers in the array
 // example: 5,5,8 has duplicate 5.
 function findDuplicateIntegers(input) {
-
-    // add logic to find missing integers in input
-    
 }
 
 // given input as an unsorted array of integers, find the smallest and largest integers in the array
 // example: 5,5,8,32,20,10 has smallest as 5 and largest as 32.
 function findSmallestAndLargestIntegers(input) {
+}
 
-    // add logic to find missing integers in input
-
+// given input as an array of integers, find all pairs of integers that add up to the targetSum value
+// example: 1,2,3,4,5,6,7,8,9 with targetSum of 8 has pairs 1:7, 2:6, and 3:5 
+function findSum(input, targetSum) {
 }
 
 module.exports.findMissingIntegers = findMissingIntegers;
 module.exports.findDuplicateIntegers = findDuplicateIntegers;
 module.exports.findSmallestAndLargestIntegers = findSmallestAndLargestIntegers;
+module.exports.findSum = findSum;
