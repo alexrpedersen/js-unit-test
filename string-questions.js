e1fcad69970c53d5ff434c7732f85d06ce1b65d8
alexrpedersen <alex.r.pedersen@outlook.com>
2020-11-13 13:08:02 -0800

    daily practice

--- string-questions.js
+++ string-questions.js
@@ -1,6 +1,23 @@
 // given input as a string, find the duplicate characters in the string
 // example: abccdefg has has a duplicate character of c
 function findDuplicateCharacters(input) {
+
+    let sameChar = [];
+
+    if (input && input.length > 0) {
+
+        input.sort((a,b) => a - b);
+
+        for (i = 0; i < input.lenth; i++) {
+
+            if (input[i] === input[i + 1]) {
+                sameChar.push(input[i]);
+            }
+        }
+
+    }
+
+    return sameChar;
 }
 
 module.exports.findDuplicateCharacters = findDuplicateCharacters;
